1953361aa960e9389591eaf88305bc2f1e17120f
FegaMega <david.smidebrant@gmail.com>
2023-07-03 10:57:33 +0200

    test (not working)

--- inputControler.py
+++ inputControler.py
@@ -67,39 +67,107 @@ def eventRIGHT(speed, Key):
 
 def inputSaver(event, Key):
     if event.type == pygame.KEYDOWN: 
-        if event.key == K_s:
-            Key[11][1] = True
-        if event.key == K_w:
-            Key[1][1] = True
-        if event.key == K_a:
-            Key[10][1] = True
-        if event.key == K_d:
-            Key[12][1] = True
-        if event.key == K_DOWN:
-            Key[28][1] = True
-        if event.key == K_UP:
-            Key[26][1] = True
-        if event.key == K_LEFT:
-            Key[27][1] = True
-        if event.key == K_RIGHT:
-            Key[29][1] = True
+        
+ #       if event.key == K_q:
+ #           Key[0][1] == True
+ #       if event.key == K_w:
+ #           Key[1][1] == True
+ #       if event.key == K_e:
+ #           Key[2][1] == True
+ #       if event.key == K_r:
+ #           Key[3][1] == True
+ #       if event.key == K_t:
+ #           Key[4][1] == True
+ #       if event.key == K_y:
+ #           Key[5][1] == True
+ #       if event.key == K_u:
+ #           Key[6][1] == True
+ #       if event.key == K_i:
+ #           Key[7][1] == True
+ #       if event.key == K_o:
+ #           Key[8][1] == True
+ #       if event.key == K_p:
+ #           Key[9][1] == True
+ #       if event.key == K_a:
+ #           Key[10][1] == True
+ #       if event.key == K_s:
+ #           Key[11][1] == True
+ #       if event.key == K_d:
+ #           Key[12][1] == True
+ #       if event.key == K_f:
+ #           Key[13][1] == True
+ #       if event.key == K_g:
+ #           Key[14][1] == True
+ #       if event.key == K_h:
+ #           Key[15][1] == True
+ #       if event.key == K_j:
+ #           Key[16][1] == True
+ #       if event.key == K_k:
+ #           Key[17][1] == True
+ #       if event.key == K_l:
+ #           Key[18][1] == True
+ #       if event.key == K_z:
+ #           Key[19][1] == True
+ #       if event.key == K_x:
+ #           Key[20][1] == True
+ #       if event.key == K_c:
+ #           Key[21][1] == True
+ #       if event.key == K_v:
+ #           Key[22][1] == True
+ #       if event.key == K_b:
+ #           Key[23][1] == True
+ #       if event.key == K_n:
+ #           Key[24][1] == True
+ #       if event.key == K_m:
+ #           Key[25][1] == True
+        for i in Key:
+            if event.key == vars(("K_", str(i[0].lower()))):
+                i[1] = True
+        
+ #      if event.key == K_s:
+ #          Key[11][1] = True
+ #      if event.key == K_w:
+ #          Key[1][1] = True
+ #      if event.key == K_a:
+ #          Key[10][1] = True
+ #      if event.key == K_d:
+ #          Key[12][1] = True
+ #      if event.key == K_DOWN:
+ #          Key[28][1] = True
+ #      if event.key == K_UP:
+ #          Key[26][1] = True
+ #      if event.key == K_LEFT:
+ #          Key[27][1] = True
+ #      if event.key == K_RIGHT:
+ #          Key[29][1] = True
+ #  if event.type == pygame.MOUSEBUTTONDOWN:
+ #      if event.button == 1:
+ #          Key[30][1] = True
+ #      if event.button == 1:
+ #          Key[31][1] = True
+
     if event.type == pygame.KEYUP:
-        if event.key == K_s:
-            Key[11][1] = False
-        if event.key == K_w:
-            Key[1][1] = False
-        if event.key == K_a:
-            Key[10][1] = False
-        if event.key == K_d:
-            Key[12][1] = False
-        if event.key == K_DOWN:
-            Key[28][1] = False
-        if event.key == K_UP:
-            Key[26][1] = False
-        if event.key == K_LEFT:
-            Key[27][1] = False
-        if event.key == K_RIGHT:
-            Key[29][1] = False
+        for i in Key:
+            if event.key == vars(("K_", str(i[0].lower()))):
+                i[1] = False
+ 
+ 
+ #       if event.key == K_s:
+ #          Key[11][1] = False
+ #      if event.key == K_w:
+ #          Key[1][1] = False
+ #      if event.key == K_a:
+ #          Key[10][1] = False
+ #      if event.key == K_d:
+ #          Key[12][1] = False
+ #      if event.key == K_DOWN:
+ #          Key[28][1] = False
+ #      if event.key == K_UP:
+ #          Key[26][1] = False
+ #      if event.key == K_LEFT:
+ #          Key[27][1] = False
+ #      if event.key == K_RIGHT:
+ #          Key[29][1] = False
 def inputHandler(user, Key):
     eventDOWN(user.speed, Key)
     eventUP(user.speed, Key) 
